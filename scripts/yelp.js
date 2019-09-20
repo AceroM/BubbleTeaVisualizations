e12803fc6eaabccf2dc7f01d786dfbd4a0bcbc9d
Miguel <miguelacero528@gmail.com>
2019-09-20 07:45:03 -0400

    updated yelp thing

--- scripts/yelp.js
+++ scripts/yelp.js
@@ -9,19 +9,40 @@ const baseUrl = "https://api.yelp.com/v3";
 const term = encodeURIComponent("Bubble Tea");
 
 const config = {
-  headers: { Authorization: "bearer " + process.env.yelpToken }
+  headers: { Authorization: "bearer " + process.env.YELP_TOKEN }
 };
 
-(function YelpToCsv() {
+(async function YelpToCsv() {
   let coordinates = [
     {
       latitude: "40.730610",
-      longitude: "-73.935242"
+      longitude: "-73.935242",
+      description: "North Manhattan"
+    },
+    {
+      latitude: "40.838197",
+      longitude: "-73.942332",
+      description: "Soho"
+    },
+    {
+      latitude: "40.765856",
+      longitude: "-73.979325",
+      description: "Central Park"
     }
   ];
 
-  const business_search = axios.get(
-    `${baseUrl}/businesses/search?term=${term}&latitude=40.730610&longitude=-73.935242&limit=50`,
-    config
-  );
+  let businesses = [];
+  for (let coord of coordinates) {
+    const store = await axios.get(
+      `${baseUrl}/businesses/search?term=${term}&latitude=${coord.latitude}&longitude=${coord.longitude}&limit=50`,
+      config
+    );
+    businesses.push(store.data.businesses);
+  }
+  businesses = businesses.flat();
+  businesses = [...new Set(businesses)];
+  console.log("businesses");
+  console.log(businesses);
+  console.log(`--------------`);
+  console.log(businesses.length);
 })();
