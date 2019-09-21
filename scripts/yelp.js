4f89b0d1ba8eadfbf3ad4fd67c8ff577c909d9cd
LeoAuyeung <leo297747@gmail.com>
2019-09-20 23:34:33 -0400

    Cleaned up YelpToCsv function to take in lat and lon

--- scripts/yelp.js
+++ scripts/yelp.js
@@ -1,48 +1,31 @@
 /**
- * This uses the yelp api to
+ * This uses the yelp api to query for 50 (limit) Bubble Tea locations near my location
  */
 
-const axios = require("axios");
-
 // https://www.yelp.com/developers/documentation/v3/business_search
+const axios = require("axios");
 const baseUrl = "https://api.yelp.com/v3";
 const term = encodeURIComponent("Bubble Tea");
-
 const config = {
   headers: { Authorization: "bearer " + process.env.YELP_TOKEN }
 };
+const latitude = "40.730610";
+const longitude = "-73.935242";
 
-(async function YelpToCsv() {
-  let coordinates = [
-    {
-      latitude: "40.730610",
-      longitude: "-73.935242",
-      description: "North Manhattan"
-    },
-    {
-      latitude: "40.838197",
-      longitude: "-73.942332",
-      description: "Soho"
-    },
-    {
-      latitude: "40.765856",
-      longitude: "-73.979325",
-      description: "Central Park"
-    }
-  ];
-
+(async function YelpToCsv(latitude, longitude) {
+  const description = "My location"
+  latitude = "40.730610"; // COMMENT THIS OUT LATER
+  longitude = "-73.935242"; // COMMENT THIS OUT LATER
+  const store = await axios.get(
+    `${baseUrl}/businesses/search?term=${term}&latitude=${latitude}&longitude=${longitude}&limit=50`,
+    config
+  );
   let businesses = [];
-  for (let coord of coordinates) {
-    const store = await axios.get(
-      `${baseUrl}/businesses/search?term=${term}&latitude=${coord.latitude}&longitude=${coord.longitude}&limit=50`,
-      config
-    );
-    businesses.push(store.data.businesses);
-  }
+  businesses.push(store.data.businesses);
   businesses = businesses.flat();
   businesses = [...new Set(businesses)];
-  console.log("businesses");
+  console.log("businesses:");
   console.log(businesses);
   console.log(`--------------`);
-  console.log(businesses.length);
+  console.log(businesses.length); // should be 50 since we set the limit to 50
 })();
