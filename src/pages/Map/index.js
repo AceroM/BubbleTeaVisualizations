ebc136243cce44ab5330d2428d25f255de68be2f
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-09-21 05:34:08 -0400

    adding basic query string line to componentdidmount

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import Logo from '../../components/Logo';
+import queryString from 'query-string'
 import './Map.scss';
 
 class Map extends Component{
+
+  componentDidMount() {
+    const values = queryString.parse(this.props.location.search)
+    
+  }
   render(){
     return(
       <div className="body-wrapper">
