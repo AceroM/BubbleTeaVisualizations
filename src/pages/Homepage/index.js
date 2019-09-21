ff5c2aeb671191f89108f0049d89c40e81283bb6
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-09-21 03:44:43 -0400

    adding input form but css isn't the greatest. please fix

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
 import './Homepage.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Homepage = () => {
-
-  return (
-    <div className="body-wrapper">
-      <div className="bmb-header">
-        <div className="grid-container">
-          <a href="/"><img className="logo" src={"/bubble-tea.png"} alt ="logo"/></a>
-        </div>
+class Homepage extends Component {
+  constructor (){
+    super();
+  }
+  render(){
+    return (
+      <div className="body-wrapper">
+          <div className="grid-container">
+            <a href="/"><img className="logo" src={"/bubble-tea.png"} alt ="logo"/></a>
+          </div>
+          <div className ="home-header-box">
+            <div>
+              <h1>Find Bubble Tea Place Near You</h1>
+            </div>
+          </div>
+          <div className = "bmb-form-box">
+            <div className = "input-group">
+              <span className = "input-group-label"><FontAwesomeIcon icon={faSearch} /></span>
+              {/* <strong>Enter your location</strong> */}
+              <input className = "input-group-field" placeholder="Enter your location" />
+            </div>
+            <input type="submit" className= "button primary white-color-text large"value="Go" />
+            
+          </div>
       </div>
-      
-    </div>
-    
-
-  )
+    )
+  }
+  
 }
 export default Homepage;
