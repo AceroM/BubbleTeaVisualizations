ab155f381f0a2b5c1478cd3e51cfed5db9c2832c
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-09-28 06:22:46 -0400

    asdf

---
+++ frontend/build/precache-manifest.78a2085f4d2c35adec83ab359989d97f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cb83f66cc3e5b02effd9bc6f49f500",
+    "url": "/AceroM/BubbleTeaVisualizations/index.html"
+  },
+  {
+    "revision": "7d7fa226090af5d25697",
+    "url": "/AceroM/BubbleTeaVisualizations/static/css/main.8a0dbceb.chunk.css"
+  },
+  {
+    "revision": "e5b14e5eae3ac2ff6a31",
+    "url": "/AceroM/BubbleTeaVisualizations/static/js/2.b2ee2be6.chunk.js"
+  },
+  {
+    "revision": "7d7fa226090af5d25697",
+    "url": "/AceroM/BubbleTeaVisualizations/static/js/main.d7430f42.chunk.js"
+  },
+  {
+    "revision": "6946603e93b4eb3e2c44",
+    "url": "/AceroM/BubbleTeaVisualizations/static/js/runtime-main.e44d8af4.js"
+  },
+  {
+    "revision": "9b0060a1c031998a217f8dc0c9591c39",
+    "url": "/AceroM/BubbleTeaVisualizations/static/media/bg-pattern-dark.9b0060a1.png"
+  },
+  {
+    "revision": "4511a8b2ed66b98f11a55524221d2a94",
+    "url": "/AceroM/BubbleTeaVisualizations/static/media/loading.4511a8b2.png"
+  },
+  {
+    "revision": "45691eb375d9eac08a2af31bb2c4e82a",
+    "url": "/AceroM/BubbleTeaVisualizations/static/media/milk.45691eb3.svg"
+  }
+]);
