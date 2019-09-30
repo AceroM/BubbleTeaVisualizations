a00bceec4d0d034a61496485de55a19cac01ff39
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-09-30 04:26:29 -0400

    making sure i include the build file

---
+++ frontend/build/precache-manifest.d9b72bddac0750031da0cfbfca82b252.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2a5b6a5cb65634a713457e4608a309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb813973f0a10de0c7d",
+    "url": "/static/css/main.c0a9f20d.chunk.css"
+  },
+  {
+    "revision": "68b755840b75af489109",
+    "url": "/static/js/2.7fce3aab.chunk.js"
+  },
+  {
+    "revision": "4cb813973f0a10de0c7d",
+    "url": "/static/js/main.28a9b0ea.chunk.js"
+  },
+  {
+    "revision": "1fdd9c20f49dbea1a27e",
+    "url": "/static/js/runtime-main.42ecad90.js"
+  },
+  {
+    "revision": "25ac9b7e7f035b1e550b94ffe675c184",
+    "url": "/static/media/404.25ac9b7e.png"
+  },
+  {
+    "revision": "9b0060a1c031998a217f8dc0c9591c39",
+    "url": "/static/media/bg-pattern-dark.9b0060a1.png"
+  },
+  {
+    "revision": "4511a8b2ed66b98f11a55524221d2a94",
+    "url": "/static/media/loading.4511a8b2.png"
+  },
+  {
+    "revision": "45691eb375d9eac08a2af31bb2c4e82a",
+    "url": "/static/media/milk.45691eb3.svg"
+  },
+  {
+    "revision": "a596b2363f85a066c69d363b5d994cbf",
+    "url": "/static/media/stateastics.a596b236.png"
+  }
+]);
