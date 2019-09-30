9f190c5b5d3ff23739b74a24f0acac1e98215b40
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-09-30 04:41:57 -0400

    updated port

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("listening on port");
 });
