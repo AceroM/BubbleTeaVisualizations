6a34f234ba6aca071b683c03db422720fd89b7d9
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-11-11 12:15:29 -0500

    changed tableau react and aligned it or w.e

--- frontend/src/components/Logo/index.js
+++ frontend/src/components/Logo/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Stateastics from '../../static/stateastics.png'
-import Wordcloud from '../../static/wordcloud.png'
-import './Logo.scss'
+import React from "react";
+import Stateastics from "../../static/stateastics.png";
+import Wordcloud from "../../static/wordcloud.png";
+import "./Logo.scss";
 
 function Logo() {
   return (
@@ -12,8 +12,11 @@ function Logo() {
       <a href="/wordcloud">
         <img className="logo" src={Wordcloud} alt="logo" />
       </a>
+      <a href="/report">
+        <h1> Tableau Maps </h1>
+      </a>
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
